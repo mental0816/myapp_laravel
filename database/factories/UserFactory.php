d519a0b97282c4b43926edfecebeaa3be7e898c1
JaeHyang <olleh53@gmail.com>
2018-03-29 14:41:30 +0900

    데이터베이스 시딩

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,12 +14,12 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
-    static $password;
+    $date = $faker->dateTimeThisMonth();
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph();
+        'created_at' => $date,
+        'updated_at' => $date,
     ];
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,9 +2,32 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>라라벨 입문!</title>
+    <title>심심할때 하는 라라벨 </title>
+    <style>
+      .test{
+        color:hotpink;
+        font-size:40px;
+        text-align:center;
+        font-family:"Hack";
+      }
+      .test:hover{
+        transition:5s;
+        color:aquamarine;
+        font-size:100px;
+        text-align:center;
+        font-family:"Hack";
+      }
+      .test:matrix{
+        color:hotpink;
+        font-size:40px;
+        text-align:center;
+        font-family:"Hack";
+      }
+    </style>
   </head>
-  <body>
+  <body style="background-color:black;">
   @yield('content')
+  <p class="test">Hello World!</p>
+  <p style="background-color:black;"></p>
   </body>
 </html>
